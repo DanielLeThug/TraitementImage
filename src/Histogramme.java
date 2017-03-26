10ac88d0e0a05f341fc3fb9cffb205d21bb05843
Idataike <julien.jakob@uha.fr>
2017-03-26 20:17:26 +0200

    Filtre Nagao
    Peut être faux

--- src/Histogramme.java
+++ src/Histogramme.java
@@ -50,9 +50,50 @@ public class Histogramme extends ShortPixmap {
     }
 
     public void filtreNagao() {
+        int pos;
         for (int i = 2; i < width - 2; i++) {
             for (int j = 2; j < height - 2; j++) {
                 // utiliser les 3 petits points de java
+                pos = 0;
+                // on stocke les 25 pixels concernés
+                short[] v = new short[25];
+                for (int k = i - 2; k < i + 3; ++k) {
+                    for (int l = j - 2; l < j + 3; ++l) {
+                        v[pos++] = data[l * width + k];
+                    }
+                }
+                
+                // tableaux contenant les moyennes et variances de chaque fenêtre
+                double[] moyennes = new double[9];
+                double[] variances = new double[9];
+                // fenêtres de Nagao
+                short[] f1 = {v[0], v[1], v[5], v[6], v[7], v[11], v[12]};
+                moyennes[0] = getMean(f1); variances[0] = getVariance(f1);
+                short[] f2 = {v[1], v[2], v[3], v[6], v[7], v[8], v[12]};
+                moyennes[1] = getMean(f1); variances[1] = getVariance(f1);
+                short[] f3 = {v[3], v[4], v[7], v[8], v[9], v[12], v[13]};
+                moyennes[2] = getMean(f1); variances[2] = getVariance(f1);
+                short[] f4 = {v[5], v[6], v[10], v[11], v[12], v[15], v[16]};
+                moyennes[3] = getMean(f1); variances[3] = getVariance(f1);
+                short[] f5 = {v[8], v[9], v[12], v[13], v[14], v[18], v[19]};
+                moyennes[4] = getMean(f1); variances[4] = getVariance(f1);
+                short[] f6 = {v[6], v[7], v[8], v[11], v[12], v[13], v[16], v[17], v[18]};
+                moyennes[5] = getMean(f1); variances[5] = getVariance(f1);
+                short[] f7 = {v[11], v[12], v[15], v[16], v[17], v[20], v[21]};
+                moyennes[6] = getMean(f1); variances[6] = getVariance(f1);
+                short[] f8 = {v[12], v[16], v[17], v[18], v[21], v[22], v[23]};
+                moyennes[7] = getMean(f1); variances[7] = getVariance(f1);
+                short[] f9 = {v[12], v[13], v[17], v[18], v[19], v[23], v[24]};
+                moyennes[8] = getMean(f1); variances[8] = getVariance(f1);
+                
+                // on récupère l'indice de la plus petite variance
+                int indice = 0;
+                for (int m = 1; m < 9; ++m) {
+                    if (variances[m] < variances[indice])
+                        indice = m;
+                }
+
+                data[j * width + i] = moyennes[indice];
             }
         }
     }
