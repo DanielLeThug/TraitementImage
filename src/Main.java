498b0543c320b13ba08f3d872a538814b488bf0a
Medhyena <link684@msn.com>
2017-03-29 14:38:07 +0200

    Commentaires de ouf

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Lancement de l'application graphique
         MyInterfaceGraphique myInterfaceGraphique = new MyInterfaceGraphique();
     }
 }
